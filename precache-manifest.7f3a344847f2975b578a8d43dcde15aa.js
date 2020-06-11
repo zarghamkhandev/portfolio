fdaf9d8e9414f4a201f6b6a6737abd82fbf5dd77
unknown <zarghamkhan980@gmail.com>
2020-06-11 13:35:44 +0200

    Updates

---
+++ precache-manifest.7f3a344847f2975b578a8d43dcde15aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7672fb69f3cd35128264decb1e6edcbd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3646e33615ccaa3e3291",
+    "url": "/portfolio/static/css/main.2fe91e05.chunk.css"
+  },
+  {
+    "revision": "831002d27cf4e29cd9ed",
+    "url": "/portfolio/static/js/2.7bf5931f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.7bf5931f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3646e33615ccaa3e3291",
+    "url": "/portfolio/static/js/main.ab443884.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "331ec49c0d78e469c42c1d814dd45838",
+    "url": "/portfolio/static/media/Avenir-Book.331ec49c.ttf"
+  },
+  {
+    "revision": "901497541657a2f24e42848bcf7fad52",
+    "url": "/portfolio/static/media/Avenir-Heavy.90149754.ttf"
+  },
+  {
+    "revision": "2090551770be22b09600a40b0b4673b7",
+    "url": "/portfolio/static/media/Avenir-Medium.20905517.ttf"
+  },
+  {
+    "revision": "7fc613aa32db9b0e37135cb29fae858d",
+    "url": "/portfolio/static/media/Avenir-MediumOblique.7fc613aa.ttf"
+  },
+  {
+    "revision": "9509d3061de2ab0bb2cf68508a5ac8b4",
+    "url": "/portfolio/static/media/background-sm.9509d306.svg"
+  },
+  {
+    "revision": "5fae3915561e8a8e745282f6f7b96b06",
+    "url": "/portfolio/static/media/footer-bg.5fae3915.svg"
+  }
+]);
